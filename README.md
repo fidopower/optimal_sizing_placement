7d19b59b9bdc2c44611cf9ac1e839d42857e9083
David P. Chassin <david.chassin@me.com>
2025-03-30 11:16:51 -0700

    Update README and code cleanup

--- README.md
+++ README.md
@@ -1,2 +1,118 @@
-# optimal_sizing_placement
- Optimal generation and capacitor sizing and placement
+# User's Guide
+
+This Marimo notebook computes the optimal powerflow solution for a GridLAB-D
+network modeled using **pypower**. The notebook is organized using 3 tabs
+(not include the **Help** tab), the **Model** tab, the **Costs** tab, and the **Results** tab. 
+
+## Model 
+
+The **Model** tab is used load the initial model. Only GridLAB-D JSON model are accepted. If you have a GLM file, you must run the command `gridlabd -C MYFILE.glm -o MYFILE.json` to complile the JSON file from the GLM file. 
+
+To upload a JSON file, click on **Open JSON model** button and select the file you wish to work with.  If you have already opened a model, the new one you select will be opened in its place, or you can click on **Click to clear files** to close the existing model.
+
+Once your model is opened, the model data will be displayed in Pandas DataFrames, one per class found in the model. Select a tab to view each class in the model. Data frames are organized by object name in rows, and object property in columns.
+
+## Costs
+
+If the model contains `capacity` objects with `generator` and `capacitor` data included, then a data frame is shown with the capacity addition costs for each bus in the model.  If no capacity data is found, then a single value for generators and capacitors is given using slider inputs.
+
+The class definition for `capacity` object is 
+
+~~~
+class capacity 
+{
+    complex generator[$/MW];
+    double capacitor[$/MW];
+}
+~~~
+
+with the parent referring to the bus at which the capacity addition costs apply. Note the generator costs are presented a complex value, with the real part corresponding to the cost of adding real power capacity and the imaginary part corresponding to the cost of added reactive power capacity.
+
+## Results
+
+Three optimization results are presented. 
+
+The **Initial optimal powerflow** (OPF) shows the result of the fast-decoupled OPF solution prior to optimal sizing and placement, if feasible. The cost of the result is given in $/MWh. If load curtailment is necessary, it will be costed at 100x the maximum generation cost found in the model.
+
+The **Optimal sizing/placement solution** (OSP) solution shows the fast-decoupled sizing and placement solution given the capacity costs.
+
+The **Final optimal powerflow** (OPF) solution shows the fast-decoupled OPF solution after optimal sizing and placement is solved.
+
+All results are presented in two parts, the **Summary Data** and the **Network Data**. The summary data include totals, means, minima, and maxima values found in the model.  The network data shows the breakdown on the results by bus and branch.
+
+Each result model can be downloaded and run in GridLAB-D using the command
+
+~~~
+gridlabd MYFILE_RESULT.json
+~~~
+
+where `RESULT` is `opf_initial`, `osp`, or `opf_final` according to which optimization is selected.
+
+# Technical Guide
+
+The OPF is the solution to the following convex optimization problem for a network having $N$ busses and $M$ branches.
+
+$\begin{array}{rll}
+    \underset{x,y,g,h,c,d}{\min} & P \sqrt{g^2+h^2} + 100 \hat{P} d
+\\
+    \textrm{subject to} 
+    & \Re(G) x - g + c + \Re(D) - d = 0 & \textrm{real power flow balance} \\
+    & \Im(G) y - h - c + \Im(D) - d \frac{\Im(D)}{\Re(D)} = 0 & \textrm{reactive power flow balance} \\
+    & x_{ref} = 0 & \textrm{reference bus voltage angle is always 0} \\
+    & y_{ref} = 1 & \textrm{reference bus voltage magnitude is always 1} \\
+    & |y-1| \le 0.05 & \textrm{bus voltages within 5\% of nominal} \\
+    & |Ix| \le F & \textrm{line flow constraints} \\
+    & 0 \le g \le \Re(S) & \textrm{real generation power constraints} \\
+    & |g| \le \Im(S) & \textrm{reactive generation power constraints} \\
+    & 0 \le c \le C & \textrm{capacity setting constraints} \\
+    & 0 \le d \le D & \textrm{load shedding constraints} \\
+\end{array}$
+
+where
+
+* $P \in \mathbb{R}^N$ is the generation price,
+* $g \in \mathbb{R}^N$ is the generator real power dispatch,
+* $h \in \mathbb{R}^N$ is the generator reactive power dispatch,
+* $\hat P\in \mathbb{R}$ is the maximum generation price,
+* $d \in \mathbb{R}^N$ is the demand curtailment,
+* $G \in \mathbb{C}^{N \times N}$ is the graph Laplacian,
+* $x \in \mathbb{R}^N$ is the voltage angle,
+* $c \in \mathbb{R}^N$ is the capacitor settings,
+* $D \in \mathbb{C}^N$ is the total demand,
+* $y \in \mathbb{R}^N$ is the voltage magnitude,
+* $I \in \mathbb{R}^{M \times N}$ is the graph incidence matrix,
+* $S \in \mathbb{C}^N$ is the generation capacity
+* $y \in \mathbb{R}^N$ is the voltage magnitude, and
+* $C \in \mathbb{R}^N$ is the capacitor capacity.
+
+The OSP is the solution to the following convex optimization problem for a network having $N$ busses and $M$ branches.
+
+$\begin{array}{rll}
+    \underset{x,y,g,h,c}{\min} & P g + Q |h| + R |c|
+\\
+    \textrm{subject to} 
+    & \Re(G) x - g + c + \Re(D)(1+d) = 0 & \textrm{real power flow balance} \\
+    & \Im(G) y - h - c + \Im(D)(1+d) = 0 & \textrm{reactive power flow balance} \\
+    & x_{ref} = 0 & \textrm{reference bus voltage angle is always 0} \\
+    & y_{ref} = 1 & \textrm{reference bus voltage magnitude is always 1} \\
+    & |y-1| \le 0.05 & \textrm{bus voltages within 5\% of nominal} \\
+    & |Ix| \le F & \textrm{line flow constraints} \\
+    & g \ge 0 & \textrm{real generation power constraint} \\
+    & c \ge 0 & \textrm{capacity setting constraint} \\
+\end{array}$
+
+where
+
+* $P \in \mathbb{R}^N$ is the generation price,
+* $g \in \mathbb{R}^N$ is the generator real power dispatch,
+* $h \in \mathbb{R}^N$ is the generator reactive power dispatch,
+* $\hat P\in \mathbb{R}$ is the maximum generation price,
+* $G \in \mathbb{C}^{N \times N}$ is the graph Laplacian,
+* $d \in \mathbb{R}$ is the demand safety margin,
+* $x \in \mathbb{R}^N$ is the voltage angle,
+* $c \in \mathbb{R}^N$ is the capacitor settings,
+* $D \in \mathbb{C}^N$ is the total demand,
+* $y \in \mathbb{R}^N$ is the voltage magnitude,
+* $I \in \mathbb{R}^{M \times N}$ is the graph incidence matrix,
+* $y \in \mathbb{R}^N$ is the voltage magnitude, and
+
--- notebook.py
+++ notebook.py
@@ -5,107 +5,116 @@ app = marimo.App(width="medium")
 
 
 @app.cell
-def _(mo):
-    # Model load
-    file = mo.ui.file(label="Load JSON model", filetypes=[".json"])
-    file
-    return (file,)
-
-
-@app.cell
-def _(header_ui, mo, model, os, pd):
-    def _noheader(x):
-        return {y: z for y, z in x.items() if y not in model.data["header"]}
-
-
-    mo.vstack(
-        [
-            mo.md("# Input Data"),
-            mo.hstack(
-                [
-                    mo.md(f"<B>Model name</B>: {os.path.split(model.name)[1]}"),
-                    header_ui,
-                ],
-            ),
-            mo.ui.tabs(
-                {
-                    x: pd.DataFrame(
-                        {
-                            y: z if header_ui.value else _noheader(z)
-                            for y, z in model.find(x, dict).items()
-                        }
-                    ).transpose()
-                    for x in list(model.data["classes"])
-                }
-            ),
-        ],
-    )
-    return
-
-
-@app.cell
-def _(mo):
-    header_ui = mo.ui.checkbox(label="Show header data")
-    return (header_ui,)
-
-
-@app.cell
-def _(N, capcost_ui, gencost_ui, mo, model, np, pd):
-    # Optimal sizing result
-    _costdata = model.find("capacity",dict)
-    if _costdata:
-        gen_cost = np.zeros(N,dtype=complex)
-        cap_cost = np.zeros(N)
-        bus_name = []
-        for x,y in _costdata.items():
-            bus_name.append(y["parent"])
-            _bus = model.property(y["parent"],"bus_i")-1
-            print(_bus)
-            gen_cost[_bus] = model.property(x,"generator")
-            cap_cost[_bus] = model.property(x,"capacitor")
-        _showcost = pd.DataFrame({"Generator ($/MW)":gen_cost,"Capacitor ($/MVAr)":cap_cost},bus_name)
-    else:
-        gen_cost = gencost_ui.value
-        cap_cost = capcost_ui.value
-        _showcost = mo.hstack([gencost_ui,capcost_ui],justify='start')
-    mo.vstack([mo.md("# Capacity costs"),_showcost])
-    return bus_name, cap_cost, gen_cost, x, y
-
-
-@app.cell
-def _(mo, optimal, original, sizing):
+def _(capacity_costs, file, input_data, mo, results_view):
+    # Main UI
     mo.ui.tabs({
-        "Initial optimal powerflow":original,
-        "Optimal sizing/placement solution":sizing,
-        "Final optimal powerflow":optimal,
+        "Model":mo.vstack([file,input_data]),
+        "Costs":capacity_costs,
+        "Results":results_view,
+        "Help":mo.md(open("README.md","r").read())
     })
     return
 
 
 @app.cell
-def _(json, mo, opf_model, os):
-    _name = os.path.split(opf_model.name)[1]
-    mo.download(
-        json.dumps(opf_model.data,indent=4),
-        mimetype="application/json",
-        filename=_name, 
-        label=f"Download optimal <U>{_name}</U>"
-    )
-    return
+def _(mo):
+    # Model load
+    file = mo.ui.file(label="Open JSON model", filetypes=[".json"])
+    return (file,)
 
 
 @app.cell
-def _(error, exception, file, gld, hint, mo):
+def _(file, header_ui, hint, mo, model, os, pd):
+    # File upload
+    def _noheader(x):
+        return {y: z for y, z in x.items() if y not in model.data["header"]}
+
+    if file.value:
+        input_data = mo.vstack(
+            [
+                mo.md("# Input Data"),
+                mo.hstack(
+                    [
+                        mo.md(f"<B>Model name</B>: {os.path.split(model.name)[1]}"),
+                        header_ui,
+                    ],
+                ),
+                mo.ui.tabs(
+                    {
+                        x: pd.DataFrame(
+                            {
+                                y: z if header_ui.value else _noheader(z)
+                                for y, z in model.find(x, dict).items()
+                            }
+                        ).transpose()
+                        for x in list(model.data["classes"])
+                    }
+                ),
+            ],
+        ) 
+    else:
+        input_data = hint("open your JSON model")
+    return (input_data,)
+
+
+@app.cell
+def _(mo):
+    # Header checkbox
+    header_ui = mo.ui.checkbox(label="Show header data")
+    return (header_ui,)
+
+
+@app.cell
+def _(N, capcost_ui, file, gencost_ui, hint, mo, model, np, pd):
+    # Optimal sizing result
+    if file.value:
+        _costdata = model.find("capacity",dict)
+        if _costdata:
+            gen_cost = np.zeros(N,dtype=complex)
+            cap_cost = np.zeros(N)
+            bus_name = []
+            for x,y in _costdata.items():
+                bus_name.append(y["parent"])
+                _bus = model.property(y["parent"],"bus_i")-1
+                gen_cost[_bus] = model.property(x,"generator")
+                cap_cost[_bus] = model.property(x,"capacitor")
+            _showcost = pd.DataFrame({"Generator ($/MW)":gen_cost,"Capacitor ($/MVAr)":cap_cost},bus_name)
+        else:
+            gen_cost = gencost_ui.value
+            cap_cost = capcost_ui.value
+            _showcost = mo.hstack([gencost_ui,capcost_ui],justify='start')
+        capacity_costs = mo.vstack([mo.md("# Capacity costs"),_showcost]) 
+    else:
+        capacity_costs = mo.vstack([file,hint("open your JSON model")])
+    return bus_name, cap_cost, capacity_costs, gen_cost, x, y
+
+
+@app.cell
+def _(file, hint, mo, optimal, original, sizing):
+    # Results UI
+    if file.value:
+        results_view = mo.ui.tabs({
+            "Initial optimal powerflow":original,
+            "Optimal sizing/placement solution":sizing,
+            "Final optimal powerflow":optimal,
+        })
+    else:
+        results_view = mo.vstack([file,hint("open your JSON model")])
+    return (results_view,)
+
+
+@app.cell
+def _(error, exception, file, gld, mo):
     # Model check
-    mo.stop(not file.value, hint("upload a JSON file"))
-    try:
-        model = gld.Model(file.contents(0).decode("utf-8"))
-    except Exception as err:
-        mo.stop(True, exception(err))
-    _check = model.validate()
-    mo.stop(_check, error(_check))
-    N = len(model.find("bus", list))
-    K = len(model.find("branch", list))
+    if file.value:
+        try:
+            model = gld.Model(file.contents(0).decode("utf-8"))
+        except Exception as err:
+            mo.stop(True, exception(err))
+        _check = model.validate()
+        mo.stop(_check, error(_check))
+        N = len(model.find("bus", list))
+        K = len(model.find("branch", list))
     return K, N, model
 
 
@@ -125,7 +134,7 @@ def _(K, N, mo, np, pd):
             result["curtailment"] = np.zeros(N)
         return mo.vstack(
             [
-                mo.md("# Summary of Results"),
+                mo.md("# Summary Data"),
                 mo.md(f"**Cost of solution**: ${result['cost']:.2f}"),
                 pd.DataFrame(
                     data={
@@ -163,7 +172,7 @@ def _(K, N, mo, np, pd):
                         "Line flow",
                     ],
                 ),
-                mo.md("# Bus/Branch Results"),
+                mo.md("# Network Data"),
                 mo.hstack(
                     [
                         pd.DataFrame(
@@ -190,17 +199,30 @@ def _(K, N, mo, np, pd):
 
 
 @app.cell
-def _(error, model, results):
+def _(error, file, json, mo, model, os, results):
     # Initial powerflow result
-    try:
-        original = results(model, model.optimal_powerflow())
-    except Exception as err:
-        original = error(err)
-    return (original,)
+    if file.value:
+        try:
+            _name = os.path.split(model.name)[1]
+            _name = os.path.splitext(_name)
+            _name = f"{_name[0]}_opf_initial.json"
+            initial_download_ui = mo.download(
+                json.dumps(model.data, indent=4),
+                mimetype="application/json",
+                filename=_name,
+                label=f"Save optimal <U>{_name}</U>",
+            )
+            original = mo.vstack([results(model, model.optimal_powerflow()),initial_download_ui])
+        except Exception as err:
+            original = error(err)
+    else:
+        original = mo.md("")
+    return initial_download_ui, original
 
 
 @app.cell
 def _(mo):
+    # Capacity costs
     gencost_ui = mo.ui.slider(
         label="<B>Generation cost</B>: ($/MW)",
         start=0,
@@ -223,36 +245,72 @@ def _(mo):
 
 
 @app.cell
-def _(cap_cost, copy, error, gen_cost, model, results):
-    osp_model = copy.deepcopy(model)
-    try:
-        sizing = results(
-            osp_model,
-            osp_model.optimal_sizing(
-                gen_cost=gen_cost,
-                cap_cost=cap_cost,
-                refresh=True,
-                update_model=True,
-            ),
-        )
-    except Exception as err:
-        error(err)
-    return osp_model, sizing
+def _(cap_cost, copy, error, file, gen_cost, json, mo, model, os, results):
+    # Optimal sizing and placement
+    if file.value:
+        osp_model = copy.deepcopy(model)
+        try:
+            _name = os.path.split(osp_model.name)[1]
+            _name = os.path.splitext(_name)
+            _name = f"{_name[0]}_osp.json"
+            osp_download_ui = mo.download(
+                json.dumps(osp_model.data, indent=4),
+                mimetype="application/json",
+                filename=_name,
+                label=f"Save optimal <U>{_name}</U>",
+            )
+            sizing = mo.vstack(
+                [
+                    results(
+                        osp_model,
+                        osp_model.optimal_sizing(
+                            gen_cost=gen_cost,
+                            cap_cost=cap_cost,
+                            refresh=True,
+                            update_model=True,
+                        ),
+                    ),
+                    osp_download_ui
+                ]
+            )
+        except Exception as err:
+            sizing = error(err)
+    else:
+        sizing = mo.md("")
+    return osp_download_ui, osp_model, sizing
 
 
 @app.cell
-def _(copy, error, osp_model, results):
+def _(copy, error, file, json, mo, os, osp_model, results):
     # Final powerflow result
-    opf_model = copy.deepcopy(osp_model)
-    try:
-        optimal = results(opf_model, opf_model.optimal_powerflow(refresh=True))
-    except Exception as err:
-        optimal = error(err)
-    return opf_model, optimal
+    if file.value:
+        opf_model = copy.deepcopy(osp_model)
+        try:
+            _name = os.path.split(opf_model.name)[1]
+            _name = os.path.splitext(_name)
+            _name = f"{_name[0]}_opf_final.json"
+            final_download_ui = mo.download(
+                json.dumps(opf_model.data, indent=4),
+                mimetype="application/json",
+                filename=_name,
+                label=f"Save optimal <U>{_name}</U>",
+            )
+            optimal = mo.vstack(
+                [
+                    results(opf_model, opf_model.optimal_powerflow(refresh=True)),
+                    final_download_ui,
+                ]
+            )
+        except Exception as err:
+            optimal = error(err)
+    else:
+        optimal = mo.md("")
+    return final_download_ui, opf_model, optimal
 
 
 @app.cell
 def _(mo):
+    # Message formatting
     def message(msg, kind=None, color="black", background="white"):
         if not kind:
             return mo.md(
@@ -283,6 +341,7 @@ def _(mo):
 
 @app.cell
 def _():
+    # Notebook setup
     import marimo as mo
     import os
     import sys
@@ -292,13 +351,12 @@ def _():
     import copy
     if os.environ["HOME"] == "/home/pyodide":
         # cannot import from requirements.txt in WASM
-        import numpy
         import cvxpy
     else:
         import subprocess
         subprocess.run([sys.executable,"-m","pip","install","-r","requirements.txt"],capture_output=True)
     import gld_pypower as gld
-    return copy, cvxpy, gld, json, mo, np, numpy, os, pd, subprocess, sys
+    return copy, cvxpy, gld, json, mo, np, os, pd, subprocess, sys
 
 
 if __name__ == "__main__":
